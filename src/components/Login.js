38e1206cbd682a72f59ead13c793be6ead1d62a8
SARB <mrsinghbusiness05@gmail.com>
2023-07-20 16:42:42 +0530

    style - Login/Signup Ready

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import '../styles/common.css';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+   const handleLogin = () => {}
+   const handleEmail = () => {}
+   const handlePassword = () => {}
+   const handleTestLogin = () => {}
+
+   const [loginData,setloginData] = useState({
+       email : "",
+       password : ""
+   })
+
   return (
-    <div>Login</div>
+    <>
+    <Header />
+
+      <section className="app-ctn">
+        <form className="br-md" onSubmit={handleLogin}>
+          <h2 className="text-center mg-bottom-md">Login</h2>
+          {/* {loginData.error && (
+            <p className="error-msg form-control">Invalid credentials</p>
+          )} */}
+          <div className="form-control">
+            <label htmlFor="email" className="fw-bold">
+              Email address
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="name@gmail.com"
+              required
+              value={loginData.email}
+              onChange={(e) => handleEmail(e)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="password" className="fw-bold">
+              Password
+            </label>
+            <input
+              type="text"
+              name="password"
+              id="password"
+              required
+              value={loginData.password}
+              onChange={(e) => handlePassword(e)}
+              placeholder="&lowast;&lowast;&lowast;&lowast;&lowast;&lowast;&lowast;&lowast;"
+            />
+         
+          </div>
+          <div className="form-control">
+            <input type="checkbox" id="remember-me" name="checkbox" />
+            <label htmlFor="remember-me" className="fw-bold">
+              Remember me{" "}
+            </label>
+            <Link to="/forgotpassword" className="forgot-pw fw-bold">
+              Forgot your Password?
+            </Link>
+          </div>
+          <div className="form-control">
+            <button className="btn btn-primary">Login</button>
+          </div>
+          <div className="form-control">
+            <button
+              className="btn btn-primary-outline"
+              onClick={(e) => handleTestLogin(e)}
+            >
+              Fill test credentials
+            </button>
+          </div>
+          <div className="account-toggle fw-bold">
+            <Link to="/signup">
+              Create New Account{" "}
+            </Link>
+          </div>
+        </form>
+      </section>
+
+    <Footer />
+    </>
   )
 }
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,8 +1,123 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Header from './Header'
+import { Link } from 'react-router-dom'
+import '../styles/common.css';
 
 const Signup = () => {
+
+   const [signUpData,setSignUpData] = useState({
+      firstName :"",
+      lastName :"",
+      email :"",
+      password : "",
+      confirmPassword : "",
+   });
+
+   const handleSignUp = () => {}
+   const handleSignUpData = () => {}
+
+   const handleTestSignup = () => {}
+
   return (
-    <div>Signup</div>
+    <>
+     <Header />
+
+     <section className="app-ctn">
+        <form className="br-md" onSubmit={handleSignUp}>
+          <h2 className="text-center mg-bottom-md">Signup</h2>
+          <div className="form-control">
+            <label htmlFor="first-name" className="fw-bold">
+              First name
+            </label>
+            <input
+              type="text"
+              id="first-name"
+              name="firstName"
+              placeholder="first name"
+              required
+              value={signUpData.firstName}
+              onChange={(e) => handleSignUpData(e)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="last-name" className="fw-bold">
+              Last name
+            </label>
+            <input  type="text"  id="last-name"
+              name="lastName" placeholder="last name" required
+              value={signUpData.lastName}
+              onChange={(e) => handleSignUpData(e)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="email" className="fw-bold">
+              Email address
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="name@gmail.com"
+              required
+              value={signUpData.email}
+              onChange={(e) => handleSignUpData(e)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="password" className="fw-bold">
+              Password
+            </label>
+            <input
+              type="text"
+              id="password"
+              name="password"
+              placeholder="&lowast;&lowast;&lowast;&lowast;&lowast;&lowast;&lowast;&lowast;"
+              required
+              value={signUpData.password}
+              onChange={(e) => handleSignUpData(e)}
+            />
+          
+          </div>
+          <div className="form-control">
+            <label htmlFor="confirm-password" className="fw-bold">
+              Confirm password
+            </label>
+            <input
+              type="text"
+              id="confirm-password"
+              name="confirmPassword"
+              placeholder="&lowast;&lowast;&lowast;&lowast;&lowast;&lowast;&lowast;&lowast;"
+              required
+              value={signUpData.confirmPassword}
+              onChange={(e) => handleSignUpData(e)}
+            />
+          </div>
+          <div className="form-control">
+            <input type="checkbox" id="terms-condition" name="checkbox" />
+            <label htmlFor="terms-condition" className="fw-bold">
+              I accept all Terms & Conditions
+            </label>
+          </div>
+          <div className="form-control">
+            <button className="btn btn-primary">Create New Account</button>
+          </div>
+          <div className="form-control">
+            <button
+              type="button"
+              className="btn btn-primary-outline"
+              onClick={handleTestSignup}
+            >
+              Fill test credentials
+            </button>
+          </div>
+          <div className="account-toggle fw-bold">
+            <Link to="/login">
+              Already have an account{" "}
+            </Link>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
