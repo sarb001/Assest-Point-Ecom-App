5125fd22538977a64041582013f7e42b1c085feb
SARB <mrsinghbusiness05@gmail.com>
2023-07-24 05:18:15 +0530

    Error - solving for cart

--- src/Reducers/CartReducer.js
+++ src/Reducers/CartReducer.js
@@ -4,11 +4,26 @@ export const CartReducer = (state,action) => {
         case "ADD_TO_CART": 
         return {
             ...state,
-
+            cart :  action.payload.cart,
+            totalItems : state.totalItems + 1,
+            totalPrice : state.totalPrice + action.payload.price,
+        }
+        case "REMOVE_FROM_CART" : 
+        return {
+            ...state,
+            cart : action.payload.cart,
+            totalItems : state.totalItems - action.payload.quantity,
+            totalPrice :  state.totalPrice - action.payload.price * action.payload.quantity,  
         }
         case "ADD_TO_WISHLIST" : 
         return {
-            ...state,
+             ...state ,
+             wishlist : action.payload ,
+        }
+        case "REMOVE_FROM_WISHLIST" : 
+        return {
+             ...state ,
+             wishlist : action.payload ,
         }
     }
 }
